876c5dfa76c821b8a53b26c4a011ee77dae1e377
bgeijtenbeek <91257517+bgeijtenbeek@users.noreply.github.com>
2024-08-29 16:19:51 +0200

    Version update
    Updated the script to v2 (added script support as well)

--- WIN - IME Clear Cache/README.txt
+++ WIN - IME Clear Cache/README.txt
@@ -2,22 +2,33 @@
 Usage info
 ###################
 
-Script clears the Company Portal / IntuneManagementExtension cache. This can be useful in cases where installfiles get stuck in the staging phase or when a required app is in GRS mode (not responding to sync so no way of "forcing" the installer to run again). When resyncing after the script is run it will force Intune to do the detection from a clean-slate perspective.
+Script clears the Company Portal / IntuneManagementExtension cache. This can be useful in cases where installfiles get stuck in the staging phase or when a required app is in GRS mode (not responding to sync so no way of "forcing" the installer to run again). Same goes for scripts that, once run, don't do anything anymore.
+When resyncing after the script is done it will force Intune to do the detection from a clean-slate perspective and check & run everything as if it's the first time.
+It will do so for ALL USERS!
 
 This script requires no preinstalled modules and it needs to be run by an admin account.
 
 The script:
 - Kills Company Portal when open.
 - Stops the IntuneManagementExtension service.
+-- When Apps are targeted:
 - Deletes all the regkeys that contain the install status of apps
 - Takes ownership of the incoming, staging and detectionScripts folder and deletes them.
+-- When Scripts are targeted:
+- Deletes all the regkeys that contain the run status of scripts
 - Restarts the IntuneManagementExtension service.
 
 There are 2 switches that can be used in conjunction with this script:
 -Log : Writes a transcript of the process to C:\Temp\ScriptLogs\IME-ClearCache-<datestamp>.log
 -Silent : This will disable a windows popup that usually shows when the script is finished. Ideal for remote usage.
+-All : Will select both apps & script statuses to be purged
+-Apps : Will select app statuses to be purged
+-Scripts : Will select script statuses to be purged
 
 Examples:
 .\imeClearCache.ps1
 .\imeClearCache.ps1 -Log -Silent
+.\imeClearCache.ps1 -Log -Silent -Apps
+.\imeClearCache.ps1 -Log -Silent -All
+
 
--- WIN - IME Clear Cache/imeClearCache.ps1
+++ WIN - IME Clear Cache/imeClearCache.ps1
@@ -23,7 +23,17 @@ param(
      [switch]$Log,
 
      [Parameter()]
-     [switch]$Silent
+     [switch]$Silent,
+
+     [Parameter()]
+     [switch]$All,
+
+     [Parameter()]
+     [switch]$Apps,
+
+     [Parameter()]
+     [switch]$Scripts
+
  )
 
 #Set dateStamp variable
@@ -35,123 +45,159 @@ if ($Log.IsPresent){
 
 Try {
 
-    ########################################################
-    #START EMPTYING THE CACHE
-    ########################################################
-
-    #1: Check if Company Portal is opened and close it if true. 
-    Write-Host "1: Closing Company Portal if currently open.."
+    #Check if Company Portal is opened and close it if true. 
+    Write-Host "Closing Company Portal if currently open.."
     $companyPortal = Get-Process "CompanyPortal" -ErrorAction SilentlyContinue
     if ($companyPortal) {
         $companyPortal | Stop-Process -Force
         Start-Sleep -s 1
-        Write-Host "1: App was open. Closed now. Proceeding to phase 2.."
+        Write-Host "- App was open. Closed now. Proceeding.."
         } else {
-        Write-Host "1: App was not open. Proceeding to phase 2.."
+        Write-Host "- App was not open, no need for closing app in advance. Proceeding.."
         }
     Remove-Variable companyPortal
 
-    #2: Stop Intune Managament Extension Service
-    Write-Host "2: Stopping IntuneManagementExtension Service"
+    #Stop Intune Managament Extension Service
+    Write-Host "Stopping IntuneManagementExtension Service"
     Stop-Service -Name IntuneManagementExtension
     Start-Sleep -s 6
-    Write-Host "2: IntuneManagementExtension Service stopped. Proceeding with phase 3.."
+    Write-Host "- IntuneManagementExtension Service stopped. Proceeding.."
 
-    #3: Delete regkeys that make up part of the Company Portal cache.
-    Write-Host "3: Deleting regkeys that are part of the Company Portal cache.."
-    $allSubKeys = Get-ChildItem -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\IntuneManagementExtension\Win32Apps\ -Exclude "Reporting" -ErrorAction SilentlyContinue
-    #If value is not equal to NULL, delete all subkeys 
-    if ($allSubKeys) {
-        Get-ChildItem -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\IntuneManagementExtension\Win32Apps\ -Exclude "Reporting"| Remove-Item -Force -Recurse -ErrorAction SilentlyContinue
-        #See if it has worked and write to host
-        Start-Sleep -Seconds 2
-        $regKeyDelete = Get-ChildItem -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\IntuneManagementExtension\Win32Apps\ -Exclude "Reporting" -ErrorAction SilentlyContinue
-        if ($regKeyDelete) {
-            Write-Host "3: Keys were found but failed to delete. Please manually check HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\IntuneManagementExtension\Win32Apps\ and delete all subkeys. Proceeding to phase 4.."
-            Remove-Variable regKeyDelete
+    #Clear Apps cache when parameters are present
+    if ($All.IsPresent -or $Apps.IsPresent){
+        
+        ########################################################
+        #START EMPTYING THE APPS CACHE
+        ########################################################
+        Write-Host "Deleting app cache.."
+
+        #Delete regkeys that make up part of the Company Portal cache.
+        Write-Host "Deleting regkeys that are part of the Company Portal cache.."
+        $allSubKeys = Get-ChildItem -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\IntuneManagementExtension\Win32Apps\ -Exclude "Reporting" -ErrorAction SilentlyContinue
+        #If value is not equal to NULL, delete all subkeys 
+        if ($allSubKeys) {
+            Get-ChildItem -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\IntuneManagementExtension\Win32Apps\ -Exclude "Reporting"| Remove-Item -Force -Recurse -ErrorAction SilentlyContinue
+            #See if it has worked and write to host
+            Start-Sleep -Seconds 2
+            $regKeyDelete = Get-ChildItem -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\IntuneManagementExtension\Win32Apps\ -Exclude "Reporting" -ErrorAction SilentlyContinue
+            if ($regKeyDelete) {
+                Write-Host "- Keys were found but failed to delete. Please manually check HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\IntuneManagementExtension\Win32Apps\ and delete all subkeys. Proceeding.."
+                Remove-Variable regKeyDelete
+            } else {
+                Write-Host "- Keys were found and successfully deleted. Proceeding.." 
+            }
         } else {
-            Write-Host "3: Keys were found and successfully deleted. Proceeding to phase 4.." 
+            Write-Host "- No keys were found. Proceeding.."
         }
-    } else {
-        Write-Host "3: No keys were found. Proceeding with phase 4.."
-    }
-    Remove-Variable allSubKeys 
+        Remove-Variable allSubKeys 
   
-    #4: Remove Content\Incoming folder.
-    Write-Host "4: Remove the Incoming folder that is part of the Company Portal cache.."
-    $checkIncoming = Get-Item 'C:\Program Files (x86)\Microsoft Intune Management Extension\Content\Incoming' -ErrorAction SilentlyContinue
-    if ($checkIncoming) {
-        #take ownership for admin group
-        takeown /a /r /d Y /f "C:\Program Files (x86)\Microsoft Intune Management Extension\Content\Incoming"
-        #First remove items, then remove the folder
-        Get-ChildItem "C:\Program Files (x86)\Microsoft Intune Management Extension\Content\Incoming" -Include *.* -Recurse | ForEach  { $_.Delete()}
-        Remove-Item -Path "C:\Program Files (x86)\Microsoft Intune Management Extension\Content\Incoming" -Force -Recurse -ErrorAction SilentlyContinue    
-        Start-Sleep -s 3
-        #has it worked?
+        #Remove Content\Incoming folder.
+        Write-Host "Remove the Content\Incoming folder that is part of the Company Portal cache.."
         $checkIncoming = Get-Item 'C:\Program Files (x86)\Microsoft Intune Management Extension\Content\Incoming' -ErrorAction SilentlyContinue
         if ($checkIncoming) {
-            Write-Host "4: The Incoming folder was found but deletion failed. Please check the C:\Program Files (x86)\Microsoft Intune Management Extension\Content\Incoming folder for manual deletion. Proceeding with phase 5.." 
+            #take ownership for admin group
+            takeown /a /r /d Y /f "C:\Program Files (x86)\Microsoft Intune Management Extension\Content\Incoming"
+            #First remove items, then remove the folder
+            Get-ChildItem "C:\Program Files (x86)\Microsoft Intune Management Extension\Content\Incoming" -Include *.* -Recurse | ForEach  { $_.Delete()}
+            Remove-Item -Path "C:\Program Files (x86)\Microsoft Intune Management Extension\Content\Incoming" -Force -Recurse -ErrorAction SilentlyContinue    
+            Start-Sleep -s 3
+            #has it worked?
+            $checkIncoming = Get-Item 'C:\Program Files (x86)\Microsoft Intune Management Extension\Content\Incoming' -ErrorAction SilentlyContinue
+            if ($checkIncoming) {
+                Write-Host "- The Content\Incoming folder was found but deletion failed. Please check the C:\Program Files (x86)\Microsoft Intune Management Extension\Content\Incoming folder for manual deletion. Proceeding.." 
+            } else {
+                Write-Host "- The Content\Incoming folder was found and successfully deleted. Proceeding.." 
+            }
         } else {
-            Write-Host "4: The Incoming folder was found and successfully deleted. Proceeding with phase 5.." 
+            Write-Host "- The Content\Incoming folder was not found, no deletion required. Proceeding.."
         }
-    } else {
-        Write-Host "4: The Incoming folder was not found, no deletion required. Proceeding with phase 5.."
-    }
-    Remove-Variable checkIncoming 
+        Remove-Variable checkIncoming 
 
-    #5: Remove Content\Staging folder.
-    Write-Host "5: Remove the Staging folder that is part of the Company Portal cache.."
-    $checkStaging = Get-Item 'C:\Program Files (x86)\Microsoft Intune Management Extension\Content\Staging' -ErrorAction SilentlyContinue
-    if ($checkStaging) {
-        #take ownership for admin group
-        takeown /a /r /d Y /f "C:\Program Files (x86)\Microsoft Intune Management Extension\Content\Staging"
-        #First remove items, then remove the folder
-        Get-ChildItem "C:\Program Files (x86)\Microsoft Intune Management Extension\Content\Staging" -Include *.* -Recurse | ForEach  { $_.Delete()}
-        Remove-Item -Path "C:\Program Files (x86)\Microsoft Intune Management Extension\Content\Staging" -Force -Recurse -ErrorAction SilentlyContinue    
-        Start-Sleep -s 3
-        #has it worked?
+        #Remove Content\Staging folder.
+        Write-Host "Remove the Content\Staging folder that is part of the Company Portal cache.."
         $checkStaging = Get-Item 'C:\Program Files (x86)\Microsoft Intune Management Extension\Content\Staging' -ErrorAction SilentlyContinue
         if ($checkStaging) {
-            Write-Host "5: The Staging folder was found but deletion failed. Please check the C:\Program Files (x86)\Microsoft Intune Management Extension\Content\Staging folder for manual deletion. Proceeding with phase 6.." 
+            #take ownership for admin group
+            takeown /a /r /d Y /f "C:\Program Files (x86)\Microsoft Intune Management Extension\Content\Staging"
+            #First remove items, then remove the folder
+            Get-ChildItem "C:\Program Files (x86)\Microsoft Intune Management Extension\Content\Staging" -Include *.* -Recurse | ForEach  { $_.Delete()}
+            Remove-Item -Path "C:\Program Files (x86)\Microsoft Intune Management Extension\Content\Staging" -Force -Recurse -ErrorAction SilentlyContinue    
+            Start-Sleep -s 3
+            #has it worked?
+            $checkStaging = Get-Item 'C:\Program Files (x86)\Microsoft Intune Management Extension\Content\Staging' -ErrorAction SilentlyContinue
+            if ($checkStaging) {
+                Write-Host "- The Content\Staging folder was found but deletion failed. Please check the C:\Program Files (x86)\Microsoft Intune Management Extension\Content\Staging folder for manual deletion. Proceeding.." 
+            } else {
+                Write-Host "- The Content\Staging folder was found and successfully deleted. Proceeding.." 
+            }
         } else {
-            Write-Host "5: The Staging folder was found and successfully deleted. Proceeding with phase 6.." 
+            Write-Host "- Content\Staging folder not found, no deletion required. Proceeding.."
         }
-    } else {
-        Write-Host "5: Staging folder not found, no deletion required. Proceeding with phase 6.."
-    }
-    Remove-Variable checkStaging
+        Remove-Variable checkStaging
 
-    #6: Remove Content\DetectionScripts folder.
-    Write-Host "6: Remove the DetectionScripts folder that is part of the Company Portal cache.."
-    $checkDetectionScripts = Get-Item 'C:\Program Files (x86)\Microsoft Intune Management Extension\Content\DetectionScripts' -ErrorAction SilentlyContinue
-    if ($checkDetectionScripts) {
-        #take ownership for admin group
-        takeown /a /r /d Y /f "C:\Program Files (x86)\Microsoft Intune Management Extension\Content\DetectionScripts"
-        #First remove items, then remove the folder
-        Get-ChildItem "C:\Program Files (x86)\Microsoft Intune Management Extension\Content\DetectionScripts" -Include *.* -Recurse | ForEach  { $_.Delete()}
-        Remove-Item -Path "C:\Program Files (x86)\Microsoft Intune Management Extension\Content\DetectionScripts" -Force -ErrorAction SilentlyContinue    
-        Start-Sleep -s 3
-        #has it worked?
+        #Remove Content\DetectionScripts folder.
+        Write-Host "Remove the Content\DetectionScripts folder that is part of the Company Portal cache.."
         $checkDetectionScripts = Get-Item 'C:\Program Files (x86)\Microsoft Intune Management Extension\Content\DetectionScripts' -ErrorAction SilentlyContinue
         if ($checkDetectionScripts) {
-            Write-Host "6: The DetectionScripts folder was found but deletion failed. Please check the C:\Program Files (x86)\Microsoft Intune Management Extension\Content\DetectionScripts folder for manual deletion. Proceeding with phase 7.." 
+            #take ownership for admin group
+            takeown /a /r /d Y /f "C:\Program Files (x86)\Microsoft Intune Management Extension\Content\DetectionScripts"
+            #First remove items, then remove the folder
+            Get-ChildItem "C:\Program Files (x86)\Microsoft Intune Management Extension\Content\DetectionScripts" -Include *.* -Recurse | ForEach  { $_.Delete()}
+            Remove-Item -Path "C:\Program Files (x86)\Microsoft Intune Management Extension\Content\DetectionScripts" -Force -ErrorAction SilentlyContinue    
+            Start-Sleep -s 3
+            #has it worked?
+            $checkDetectionScripts = Get-Item 'C:\Program Files (x86)\Microsoft Intune Management Extension\Content\DetectionScripts' -ErrorAction SilentlyContinue
+            if ($checkDetectionScripts) {
+                Write-Host "- The Content\DetectionScripts folder was found but deletion failed. Please check the C:\Program Files (x86)\Microsoft Intune Management Extension\Content\DetectionScripts folder for manual deletion. Proceeding.." 
+            } else {
+                Write-Host "- The Content\DetectionScripts folder was found and successfully deleted. Proceeding.." 
+            }
         } else {
-            Write-Host "6: The DetectionScripts folder was found and successfully deleted. Proceeding with phase 7.." 
+            Write-Host "- Content\DetectionScripts folder not found, no deletion required. Proceeding.."
         }
-    } else {
-        Write-Host "6: DetectionScript folder not found, no deletion required. Proceeding with phase 7.."
+        Remove-Variable checkDetectionScripts
+        Write-Host "Finished deleting everything related to apps."
     }
-    Remove-Variable checkDetectionScripts
 
-    #7: Start Intune Managament Extension Service
-    Write-Host "7: Starting IntuneManagementExtension Service"
+    #Clear Scripts cache when parameters are present
+    if ($All.IsPresent -or $Scripts.IsPresent){ 
+
+        ########################################################
+        #START EMPTYING THE SCRIPTS CACHE
+        ########################################################
+        Write-Host "Deleting scripts cache.."
+
+        #Delete regkeys that make up part of the Company Portal cache.
+        Write-Host "Deleting regkeys that are part of the Company Portal cache.."
+        $allScriptSubKeys = Get-ChildItem -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\IntuneManagementExtension\Policies\ -ErrorAction SilentlyContinue
+        #If value is not equal to NULL, delete all subkeys 
+        if ($allScriptSubKeys) {
+            Get-ChildItem -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\IntuneManagementExtension\Policies\ | Remove-Item -Force -Recurse -ErrorAction SilentlyContinue
+            #See if it has worked and write to host
+            Start-Sleep -Seconds 2
+            $scriptRegKeyDelete = Get-ChildItem -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\IntuneManagementExtension\Policies\ -ErrorAction SilentlyContinue
+            if ($scriptRegKeyDelete) {
+                Write-Host "- Keys were found but failed to delete. Please manually check HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\IntuneManagementExtension\Policies\ and delete all subkeys. Proceeding.."
+                Remove-Variable scriptRegKeyDelete
+            } else {
+                Write-Host "- Keys were found and successfully deleted. Proceeding.." 
+            }
+        } else {
+            Write-Host "- No keys were found. Proceeding.."
+        }
+        Remove-Variable allScriptSubKeys 
+        Write-Host "Finished deleting everything related to scripts."
+    }
+
+    #Start Intune Managament Extension Service
+    Write-Host "Starting IntuneManagementExtension Service"
     Start-Service -Name IntuneManagementExtension
     Start-Sleep -s 6
-    Write-Host "7: IntuneManagementExtension Service started."
+    Write-Host "- IntuneManagementExtension Service started."
 
     if (!($Silent.IsPresent)){
         #Show popup that script is done
-        Write-Host "8: Script finished, showing popup."
+        Write-Host "Script finished, showing popup."
         $wshell = New-Object -ComObject Wscript.Shell
         $wshell.Popup("The operation was completed. Please attempt another synchronization to fetch the latest data/apps/scripts from Intune.",0,"IntuneManagementExtension - ClearCache",0x0)
     }
@@ -165,17 +211,16 @@ Catch {
 
 # Exit the script or take other appropriate action
 if (!($ErrorText)) {
-    Write-Host "Exit code 0: No Error detetcted."
+    Write-Host "Exit code 0: No Error detected."
     if ($Log.IsPresent){
         Stop-Transcript
     }
     Exit 0
 }
 else {
-    Write-Host "Exit code 1: Error detetcted."
+    Write-Host "Exit code 1: Error detected."
     if ($Log.IsPresent){
         Stop-Transcript
     }
     exit 1
 }
-
